41b68fc40e96e498bf2dda88d7256f08fc0a304d
口渴不喝水 <114642005+thirst-without-water@users.noreply.github.com>
2023-04-06 16:40:48 +0800

    Update zzliyym_jiegouti.c

--- zzliyym_jiegouti/zzliyym_jiegouti/zzliyym_jiegouti.c
+++ zzliyym_jiegouti/zzliyym_jiegouti/zzliyym_jiegouti.c
@@ -1,6 +1,5 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-A。主要内容：结构体类型的声明、结构体初始化、结构体成员访问、结构体传参
+A。主要内容：结构体类型的声明、结构体成员访问、结构体传参
 一.结构体的声明
 i).结构是一些值的集合，这些值称为成员变量。结构的每个成员可以是不同类型的变量。
 struct tag
@@ -65,4 +64,32 @@ int main()
 	
 	return 0;
 }
+//
+//
 二.结构体成员访问
+void print1(struct Peo p)
+{
+	printf("%s %s %s %d\n",p.name,p.tele,p.sex,p.high); /*结构体变量.成员变量*/
+}
+void print2(struct Peo* sp)
+{
+	printf("%s %s %s %d\n",sp->name,sp->tele,sp->sex,sp->high); /*结构体指针->成员变量*/
+}
+int main()
+{
+	struct Peo p1 = {“zzl”，“13778066047”，“男”，181}; /*局部变量*/ /*初始化*/
+	struct St s = {{“zzl”，“13778066047”，“男”，181},100,3.14f};
+	print1(p1);
+	print2(&p1);
+	return 0;
+}
+//
+//
+三.结构体传参
+上面的print1和print2函数哪个好些?
+	答案是:首选print2函数。
+原因:
+函数传参的时候，参数是需要压栈的
+如果传递一个结构体对象的时候，结构体过大，参数压栈的的系统开销比较大，所以会导致性能的下降。
+结论:
+结构体传参的时候，要传结构体的地址。
